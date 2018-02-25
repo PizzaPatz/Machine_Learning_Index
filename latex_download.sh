4c719f96afce1d5904228042a83a0c0602d06947
pat <patrapee.pongtana@gmail.com>
2018-02-24 20:46:30 -0800

    Pushing sh for latex and ml_essential_lib

---
+++ latex_download.sh
@@ -0,0 +1,5 @@
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+
+echo "To use, search for Texmaker on search bar"
+echo "Change to PS->PDF and View PDF on the building bar"
---
+++ ml_essential_lib_download.sh
@@ -0,0 +1,8 @@
+#############################################
+#   Requirements: pip and UNIX system       #
+#   64 Bit				    #
+#############################################
+
+bash Anaconda3.5.1.0-Linux-x86_64.sh
+conda create -n tensorflow pip python=3.6
+source activate tensorflow
